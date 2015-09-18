cf41a240a8cea5ba57372f7be1ef81801c8dd028
phil.zhang <zhangfayou@gmail.com>
2015-09-18 09:51:55 +0800

    修改BarFeed，使得能够处理多个股票

--- algotrade/barfeed/barfeed.py
+++ algotrade/barfeed/barfeed.py
@@ -4,6 +4,7 @@
 __author__ = 'phil.zhang'
 
 from abc import abstractmethod, ABCMeta
+import os
 
 import six
 import pandas as pd
@@ -11,7 +12,7 @@ import pandas as pd
 from algotrade.bar import Bar
 from algotrade.event_engine import Event
 from algotrade.event_engine import EventEngineMixin
-from algotrade.const import EVENT_TYPE, FREQUENCY
+from algotrade.const import EventType, FREQUENCY
 
 
 class BaseBarFeed(six.with_metaclass(ABCMeta), EventEngineMixin):
@@ -25,24 +26,35 @@ class BaseBarFeed(six.with_metaclass(ABCMeta), EventEngineMixin):
 
 
 class CSVBarFeed(BaseBarFeed):
-    def __init__(self):
+    def __init__(self, instruments):
+        """
+
+        :param instruments: a list of strings, instrument code
+        :return:
+        """
         super(CSVBarFeed, self).__init__()
-        self.__dataframe = None
+        self.__instruments = instruments
+        self.__dict_dataframe = dict.fromkeys(instruments)
 
     def next_bar(self):
-        for idx, row in self.__dataframe.iterrows():
-            bar_dict = dict(row)
-            bar_dict.update(frequency=FREQUENCY.DAY, date_time=row.name)
-            bar_ = Bar(date_time=bar_dict['date_time'],
-                       open_=bar_dict['open'],
-                       high=bar_dict['high'],
-                       low=bar_dict['low'],
-                       close=bar_dict['close'],
-                       adj_close=bar_dict['adj close'],
-                       volume=bar_dict['volume'],
-                       frequency=bar_dict['frequency']
-                       )
-            yield bar_
+
+        generators = dict([(instrument, self.__dict_dataframe[instrument].iterrows()) for instrument in self.__instruments])
+        while True:
+            for instrument, gen in generators.items():
+                idx, row = gen.next()
+                bar_dict = dict(row)
+                # bar_dict.update(frequency=FREQUENCY.DAY, date_time=row.name)
+                bar_ = Bar(date_time=row.name,
+                           open_=bar_dict['open'],
+                           high=bar_dict['high'],
+                           low=bar_dict['low'],
+                           close=bar_dict['close'],
+                           adj_close=bar_dict['adj_close'],
+                           volume=bar_dict['volume'],
+                           frequency=FREQUENCY.DAY,
+                           instrument=instrument
+                           )
+                yield bar_
 
     def __next_bar(self):
         # try:
@@ -50,20 +62,27 @@ class CSVBarFeed(BaseBarFeed):
         # except StopIteration:
         #     pass
         # else:
-        #     self.event_engine.put(Event(EVENT_TYPE.EVENT_BAR_ARRIVE, bar))
+        #     self.event_engine.put(Event(EventType.EVENT_BAR_ARRIVE, bar))
         #     return bar
         row = self.next_bar().next()
-        self.event_engine.put(Event(type_=EVENT_TYPE.EVENT_BAR_ARRIVE, dict_=row))
+        self.event_engine.put(Event(type_=EventType.EVENT_BAR_ARRIVE, dict_=row))
         # return row
 
+    def __load_data_from_csv(self, path, instrument):
+        filename = instrument + '.csv'
+        p = os.path.join(os.path.abspath(path), filename)
+        print(p)
+        self.__dict_dataframe[instrument] = pd.read_csv(p, index_col=0, parse_dates=True).sort()
+        self.__dict_dataframe[instrument].columns = self.__dict_dataframe[instrument].columns.str.lower().str.replace(' ', '_')
+        print self.__dict_dataframe[instrument].columns
+
     def load_data_from_csv(self, path):
-        self.__dataframe = pd.read_csv(path, index_col=0, parse_dates=True).sort()
-        self.__dataframe.columns = [str.lower(col_names) for col_names in self.__dataframe.columns]
+        [self.__load_data_from_csv(path, instrument) for instrument in self.__instruments]
 
 
 if __name__ == '__main__':
 
-    csvfeed = CSVBarFeed()
-    csvfeed.load_data_from_csv('../orcl-2000.csv')
+    csvfeed = CSVBarFeed(['orcl_2000', 'orcl_2000_copy'])
+    csvfeed.load_data_from_csv('..\\')
     for item in csvfeed.next_bar():
         print(item)
